3150267653598425bae58d7e5224e6b0a6a8465f
Collin DeWaters <ctdewaters@bounceee.com>
2017-06-06 16:12:13 -0700

    Initial Commit

---
+++ CoreMLScenes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoreMLScenes
+//
+//  Created by Collin DeWaters on 6/6/17.
+//  Copyright © 2017 Collin DeWaters. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CoreMLScenes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoreMLScenes
+//
+//  Created by Collin DeWaters on 6/6/17.
+//  Copyright © 2017 Collin DeWaters. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
